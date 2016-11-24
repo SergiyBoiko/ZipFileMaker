95ff478ebb45de5ae6285a033088ca000633adff
SergiyBoiko <codedva00@gmail.com>
2016-11-24 11:58:00 +0200

    add Config class

---
+++ src/config/Config.java
@@ -0,0 +1,18 @@
+package config;
+public class Config {
+	public static String [] audioFormat = new String[]{"mp3", "wav", "wma"};
+	public static String [] videoFormat = new String[]{"avi", "mp4", "flv"};
+	public static String [] imageFormat = new String[]{"jpeg", "jpg", "gif", "png"};
+
+	public static String workingDirectory = "D:\\Work\\worksKepler\\FileZipMaker\\testZip";
+
+	public static String imageZip;
+	public static String audioZip;
+	public static String videoZip;
+
+	static  {
+		imageZip = "images.zip";
+		audioZip = "audios.zip";
+		videoZip = "videos.zip";
+	}
+}
